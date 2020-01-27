201d0cedbcd4762c540e6d65068d8d26fa367678
jndean <josefdean@blueyonder.co.uk>
2020-01-27 23:13:01 +0000

    Add LICENSE, update setup.py with description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name="railway",
     version="0.3.0",
+    description="A time-and-memory-linearly reversible imperative "
+                "programming language",
+    long_description=long_description,
     packages=find_packages(),
+    python_requires='>=3.8',
     entry_points={
-    	"console_scripts":[
-    		"railway = lib:run"
-    	]
+        "console_scripts": [
+            "railway = lib:run"
+        ]
     }
 )
